bf9e686cb90ede7658d948e6cc66e3dc8864a02f
misi68 <47552223+misi68@users.noreply.github.com>
2019-02-27 09:23:18 +0200

    Fibonacci

--- main.cpp
+++ main.cpp
@@ -4,72 +4,33 @@ using namespace std;
 
 int main()
 {
-    int V[100],N,M,i,D[100],S=0,uszam,ujszam=0,tar;
-
-    cout << "N=";
-    cin >>N;
-    tar=S;
 
 
 
 
 
-    for(i=0;i<N;i++){
 
 
-      cin>>V[i];
+
+int n;
+    cout << "n = ";
+    cin >> n;
+
+    int nr_2, nr_1, nr_0;
+    nr_0 = 1;
+    nr_1 = 1;
+
+    cout << nr_0 << "\n" << nr_1 << "\n";
+
+    for(int i = 3; i <= n; i++)
+    {
+        nr_2 = nr_1 + nr_0;
+        nr_0 = nr_1;
+        nr_1 = nr_2;
+
+        cout << nr_2 << "\n";
     }
 
-  cout <<"M=";
-   cin >>M;
-
-    for(i=0;i<M;i++){
-
-      cin>>D[i];
-
-    }
-
-    if(N>M){
-     for(i=0;i<M;i++){
-        if(V[i]==D[i]){
-          S=S+V[i];
-        }
-       }
-     }
-     if(N<M){
-      for(i=0;i<N;i++){
-        if(V[i]==D[i]){
-          S=S+V[i];
-      }
-     }
-     }
-
-      if(N==M){
-      for(i=0;i<N;i++){
-        if(V[i]==D[i]){
-          S=S+V[i];
-      }
-     }
-     }
-
-
-   while(S>0){
-
-
-    uszam=S%10;
-    ujszam=ujszam*10+uszam;
-    S=S/10;
-
-
-
-   }
-
-
-   if(tar==ujszam){
-
-    cout<<tar;
-
-   }
 
 
 
